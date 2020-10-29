51d1bd09e0cbeb7030d5fe16328462e1b6c3681f
Anders Pitman <tapitman11@gmail.com>
2020-10-29 17:40:07 -0600

    Add script for installing golang

---
+++ install_go.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export VERSION=1.15 OS=linux ARCH=amd64
+
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+rm go$VERSION.$OS-$ARCH.tar.gz
+
+echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
