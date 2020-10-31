df0fec84a380f1583c62636fe753a0e91b815b2b
Anders Pitman <tapitman11@gmail.com>
2020-10-30 19:36:27 -0600

    Use curl to install go

--- install_go.sh
+++ install_go.sh
@@ -2,8 +2,8 @@
 
 export VERSION=1.15 OS=linux ARCH=amd64
 
-wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+curl -O https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
 rm go$VERSION.$OS-$ARCH.tar.gz
 
 echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.bashrc
