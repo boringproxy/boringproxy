4b4dc942225208093c5acd47a4fa7e11fa88309e
Anders Pitman <tapitman11@gmail.com>
2020-11-20 18:29:12 -0700

    Add android builds
    Client seems to be working when run manually with adb shell from
    /data/local/tmp/ on a Moto G Power. Server fails when trying to
    write /.local for the certs.

---
+++ scripts/build_android.sh
@@ -0,0 +1,6 @@
+
+echo Building platform android-arm
+GOOS=android GOARCH=arm CGO_ENABLED=1 CC=$HOME/Android/Sdk/ndk/21.3.6528147/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi30-clang go build -o build/boringproxy-android-arm
+
+echo Building platform android-arm64
+GOOS=android GOARCH=arm64 CGO_ENABLED=1 CC=$HOME/Android/Sdk/ndk/21.3.6528147/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android30-clang go build -o build/boringproxy-android-arm64
