349ec7997fc8a76f139c0f131d5648b91b94cdd1
Anders Pitman <tapitman11@gmail.com>
2021-02-15 12:24:12 -0700

    Forget some stuff
    Fixes #45

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -8,13 +8,14 @@ rice embed-go
 
 cd ./cmd/boringproxy
 
+../../scripts/build_x86_64.sh linux
+../../scripts/build_x86_64.sh windows .exe
+../../scripts/build_x86_64.sh darwin
+
 ../../scripts/build_arch.sh linux 386
-../../scripts/build_arch.sh linux amd64
 ../../scripts/build_arch.sh linux arm
 ../../scripts/build_arch.sh linux arm64
 ../../scripts/build_arch.sh windows 386 .exe
-../../scripts/build_arch.sh windows amd64 .exe
-../../scripts/build_arch.sh darwin amd64
 
 mv build ../../
 cd ../../
