b887f40b93360324aa8c9e5c90bc8cd55153e86e
Anders Pitman <tapitman11@gmail.com>
2021-12-05 12:59:05 -0700

    Add freebsd and openbsd builds

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -9,12 +9,20 @@ rice embed-go
 cd ./cmd/boringproxy
 
 ../../scripts/build_x86_64.sh linux
+../../scripts/build_x86_64.sh freebsd
+../../scripts/build_x86_64.sh openbsd
 ../../scripts/build_x86_64.sh windows .exe
 ../../scripts/build_x86_64.sh darwin
 
 ../../scripts/build_arch.sh linux 386
 ../../scripts/build_arch.sh linux arm
 ../../scripts/build_arch.sh linux arm64
+../../scripts/build_arch.sh freebsd 386
+../../scripts/build_arch.sh freebsd arm
+../../scripts/build_arch.sh freebsd arm64
+../../scripts/build_arch.sh openbsd 386
+../../scripts/build_arch.sh openbsd arm
+../../scripts/build_arch.sh openbsd arm64
 ../../scripts/build_arch.sh windows 386 .exe
 
 mv build ../../
