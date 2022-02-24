dcb06497ad76635bddd52aa9cd135e1efb7afb2a
Anders Pitman <tapitman11@gmail.com>
2022-02-24 14:33:13 -0700

    Implement overriding SSH server per tunnel
    This lets you use a proxy for connecting to the SSH server, which
    is useful on networks that block SSH/port 22. For example you can
    use the boringproxy tuntls command to create a proxy that will
    tunnel the client's SSH connections over TLS to the server.
    
    It's all very meta and forces at least double encryption, but it
    could be useful.

--- api.go
+++ api.go
@@ -378,6 +378,22 @@ func (a *Api) CreateTunnel(tokenData TokenData, params url.Values) (*Tunnel, err
 		return nil, errors.New("Invalid tls-termination parameter")
 	}
 
+	sshServerAddr := a.db.GetAdminDomain()
+	sshServerAddrParam := params.Get("ssh-server-addr")
+	if sshServerAddrParam != "" {
+		sshServerAddr = sshServerAddrParam
+	}
+
+	sshServerPort := a.config.SshServerPort
+	sshServerPortParam := params.Get("ssh-server-port")
+	if sshServerPortParam != "" {
+		var err error
+		sshServerPort, err = strconv.Atoi(sshServerPortParam)
+		if err != nil {
+			return nil, errors.New("Invalid ssh-server-port parameter")
+		}
+	}
+
 	request := Tunnel{
 		Domain:           domain,
 		Owner:            owner,
@@ -389,6 +405,8 @@ func (a *Api) CreateTunnel(tokenData TokenData, params url.Values) (*Tunnel, err
 		AuthUsername:     username,
 		AuthPassword:     password,
 		TlsTermination:   tlsTerm,
+		ServerAddress:    sshServerAddr,
+		ServerPort:       sshServerPort,
 	}
 
 	tunnel, err := a.tunMan.RequestCreateTunnel(request)
