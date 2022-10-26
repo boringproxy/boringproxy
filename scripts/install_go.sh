aac0c7e41a5b60f0be82679b02e485326084e07b
Matthias Bilger <matthias@bilger.info>
2022-10-26 18:27:08 +0200

    Update go version in install script

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export VERSION=1.15 OS=linux ARCH=amd64
+export VERSION=1.19 OS=linux ARCH=amd64
 
 curl -O https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
 tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
